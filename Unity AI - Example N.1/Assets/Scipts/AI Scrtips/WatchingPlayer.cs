f7264f8f3a58f2f9ea6f0b67371048f1d56c14e4
JCharles246 <jose.munoz34@uabc.edu.mx>
2020-01-16 19:20:48 -0800

    FOV AI Behaviour
    Added a script so the AI can detect if an object is in their field of view.

---
+++ Unity AI - Example N.1/Assets/Scipts/AI Scrtips/WatchingPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchingPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    private Vector3 playerDistFromAI;
+    private Color color = Color.yellow;
+
+    public float velocity = 7.0f;
+    public float visionRange = 50.0f;
+    public float rangeFOV = 30.0f;
+
+    private float distanceToPlayer = 0.0f;
+    private float angle = 0.0f;
+
+    private bool isWatching;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isWatching = false;
+
+        //  Cuadratic distance calculations.
+        distanceToPlayer = Vector3.SqrMagnitude(transform.position - player.position);
+
+        //  Condition where we evaluate if the player is in the AI field of view.
+        if (distanceToPlayer <= Mathf.Pow(rangeFOV, 2))
+        {
+            //  Vector from AI to player.
+            playerDistFromAI = player.position - transform.position;
+
+            //  Angle Calculation.
+            angle = Vector3.Angle(transform.forward, playerDistFromAI);
+
+            //  Condition that verifies if the player is in range.
+            if (angle <= rangeFOV)
+            {
+                Debug.DrawLine(transform.position, playerDistFromAI, Color.white, 100.0f);
+                isWatching = true;
+            }
+        }
+
+        Debug.DrawLine(transform.position, playerDistFromAI, color);
+
+        if (isWatching)
+        {
+            Debug.Log("Player is being watched");
+
+            //  Some code or behaviour.
+        }
+        else
+        {
+            Debug.Log("Player is not being watched");
+        }
+    }
+}
